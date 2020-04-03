b4934e226dc13e31304c1f1b9d4c124740ce67ee
Daniel <schnolldj@gmail.com>
2020-04-03 16:40:01 -0400

    Top half of dashboard works

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -11,6 +11,12 @@ import { CalendarModule, DateAdapter } from 'angular-calendar';
 import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
 import { SwishHeaderComponent } from './swish-header/swish-header.component';
 import { SidenavComponent } from './sidenav/sidenav.component';
+import { TopDashWLstatsComponent } from './top-dash-wlstats/top-dash-wlstats.component';
+
+// Import ng-circle-progress
+import { NgCircleProgressModule } from 'ng-circle-progress';
+import { BotDashGamehistoryComponent } from './bot-dash-gamehistory/bot-dash-gamehistory.component';
+import { TopDashUpcomingComponent } from './top-dash-upcoming/top-dash-upcoming.component';
 
 @NgModule({
   declarations: [
@@ -19,12 +25,26 @@ import { SidenavComponent } from './sidenav/sidenav.component';
     CalendarComponent,
     SwishHeaderComponent,
     SidenavComponent,
+    TopDashWLstatsComponent,
+    BotDashGamehistoryComponent,
+    TopDashUpcomingComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    CalendarModule.forRoot({ provide: DateAdapter, useFactory: adapterFactory })
+    CalendarModule.forRoot({ provide: DateAdapter, useFactory: adapterFactory }),
+    
+    // Specify ng-circle-progress as an import
+    NgCircleProgressModule.forRoot({
+      // set defaults here
+      radius: 100,
+      outerStrokeWidth: 16,
+      innerStrokeWidth: 8,
+      outerStrokeColor: "#78C000",
+      innerStrokeColor: "#C7E596",
+      animationDuration: 1000
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
